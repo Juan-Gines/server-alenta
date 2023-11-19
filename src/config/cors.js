e0f008feda8f3091c32d99a8e9981b6d6d3f4963
Juan-Gines <JuangAlenta@hotmail.com>
2023-11-19 16:02:17 +0100

    configurando cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://alentaweb.vercel.app']
 
 const corsOptions = {
   origin: allowedOrigins,
