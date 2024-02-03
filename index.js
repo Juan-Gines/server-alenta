b8c49be94eddb81378dac9bf80b01e8cbfcb7763
Juan-Gines <JuangAlenta@hotmail.com>
2024-02-04 00:45:01 +0100

    mas pruebas

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-// import connectDB from '#Config/db.js'
-// import '#Config/env.js'
-// import expressApp from '#Config/express.js'
-
-// const { MONGODB_URL, MONGODB_URL_TEST, NODE_ENV } = process.env
-
-// // ? Utilizamos otra base de datos para el test
-
-// const connectionString = NODE_ENV === 'test'
-//   ? MONGODB_URL_TEST
-//   : MONGODB_URL
-
-// await connectDB(connectionString)
+import connectDB from '#Config/db.js'
+import '#Config/env.js'
 
 // const server = expressApp.listen(process.env.PORT, () => {
 //   console.log(`Servidor escuchando en el puerto http://localhost:${process.env.PORT}`)
@@ -20,6 +9,17 @@
 
 import express from 'express'
 import cors from 'cors'
+// import expressApp from '#Config/express.js'
+
+// const { MONGODB_URL, MONGODB_URL_TEST, NODE_ENV } = process.env
+
+// // ? Utilizamos otra base de datos para el test
+
+const connectionString = NODE_ENV === 'test'
+  ? MONGODB_URL_TEST
+  : MONGODB_URL
+
+await connectDB(connectionString)
 const app = express()
 app.use(express.json())
 app.use(cors())
