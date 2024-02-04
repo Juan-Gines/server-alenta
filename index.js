01441daf2b734ea5b31af1d3f06974dc5f39963e
Juan-Gines <JuangAlenta@hotmail.com>
2024-02-04 01:57:35 +0100

    seguimos probando cosas

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ import expressApp from '#Config/express.js'
 const PORT = process.env.PORT || 3000
 // Determina la URL de conexión según el entorno
 
-const server = expressApp.listen(PORT, () => {
+expressApp.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`)
 })
-
-export default server
