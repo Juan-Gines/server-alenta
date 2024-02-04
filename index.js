3ea79fe04aec378d63092c4fb188d1b620bb2202
Juan-Gines <JuangAlenta@hotmail.com>
2024-02-04 01:51:23 +0100

    sin conexion a bbdd

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-import connectDB from '#Config/db.js'
 import '#Config/env.js'
 import expressApp from '#Config/express.js'
 
-const { MONGODB_URL, MONGODB_URL_TEST, NODE_ENV, PORT } = process.env
-
+const PORT = process.env.PORT || 3000
 // Determina la URL de conexión según el entorno
-const connectionString = NODE_ENV === 'test' ? MONGODB_URL_TEST : MONGODB_URL
 
-let server
-// Conecta a la base de datos
-connectDB(connectionString)
-  .then(() => {
-    // Inicia el servidor
-    server = expressApp.listen(PORT, () => {
-      console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`)
-    })
-  })
-  .catch((error) => {
-    console.error('Error al conectar a la base de datos:', error)
-  })
+const server = expressApp.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`)
+})
 
 export default server
--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose'
 
-const connectDB = (url) => mongoose.connect(url).then(() => console.log('Database connected')).catch((error) => {
-  console.error('No se ha podido conectar a la base de datos. ', error)
-})
+const connectDB = (url) => mongoose.connect(url)
 
 export default connectDB
